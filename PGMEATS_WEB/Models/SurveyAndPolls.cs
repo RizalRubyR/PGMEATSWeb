6fcd8795ef6669d1cad4c598432a1e6ddcadf171
TosPebri <Pebri@tos.co.id>
2023-02-16 17:32:08 +0700

    <upd><Surveyandpolls><Form create>

--- PGMEATS_WEB/Models/SurveyAndPolls.cs
+++ PGMEATS_WEB/Models/SurveyAndPolls.cs
@@ -19,11 +19,17 @@ namespace PGMEATS_WEB.Models
     }
     public class SurveyAndPollsCreate
     {
-
+        public string id { get; set; }
+        public string QuestionID { get; set; }
+        public string SurveyID { get; set; }
+        public string QuestionSeqNo { get; set; }
+        public string QuestionDesc { get; set; }
+        public string ParentQuestionID { get; set; }
+        public string ParentAnswerSeqNo { get; set; }
     }
     public class SurveyAndPollsDB
     {
-        public clsResponse GetSurveyAndPolls()
+        public clsResponse GetSurveyAndPollsList()
         {
             List<SurveyAndPollsList> SurveyPollsList = new List<SurveyAndPollsList>();
             clsResponse Response = new clsResponse();
@@ -56,6 +62,96 @@ namespace PGMEATS_WEB.Models
                     Response.ID = 1;
                     Response.Message = "Success";
                     Response.Contents = SurveyPollsList;
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.ID = 0;
+                Response.Message = ex.Message;
+                Response.Contents = "";
+
+            }
+            return Response;
+        }
+
+        public clsResponse GetSurveyAndPollsCreate()
+        {
+            List<SurveyAndPollsCreate> SurveyPollsList = new List<SurveyAndPollsCreate>();
+            clsResponse Response = new clsResponse();
+            try
+            {
+                string constr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_SurveyAndPollsCreate_List", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+                    da.Dispose();
+                    cmd.Dispose();
+                    con.Close();
+
+                    SurveyPollsList = dt.AsEnumerable().Select(x =>
+                    new SurveyAndPollsCreate
+                    {
+                        id = x.Field<Int64>("id").ToString(),
+                        QuestionID = x.Field<Int64>("QuestionID").ToString(),
+                        SurveyID = x.Field<Int64>("SurveyID").ToString(),
+                        QuestionSeqNo = x.Field<string>("QuestionSeqNo"),
+                        QuestionDesc = x.Field<string>("QuestionDesc"),
+                        ParentQuestionID = x.Field<string>("ParentQuestionID"),
+                        ParentAnswerSeqNo = x.Field<string>("ParentAnswerSeqNo"),
+                    }).ToList();
+
+                    Response.ID = 1;
+                    Response.Message = "Success";
+                    Response.Contents = SurveyPollsList;
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.ID = 0;
+                Response.Message = ex.Message;
+                Response.Contents = "";
+
+            }
+            return Response;
+        }
+        public clsResponse getsurveyid()
+        {
+            int surveyid = 0;
+            clsResponse Response = new clsResponse();
+            try
+            {
+                string constr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_SurveyAndPollsCreate_SurveyID", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+                    da.Dispose();
+                    cmd.Dispose();
+                    con.Close();
+
+                    if(dt.Rows.Count > 0)
+                    {
+                        surveyid = int.Parse(dt.Rows[0]["SurveyID"].ToString()) + 1;
+                    }
+                    else
+                    {
+                        surveyid = 1;
+                    }
+
+                    Response.ID = 1;
+                    Response.Message = "Success";
+                    Response.Contents = surveyid;
                 }
             }
             catch (Exception ex)
