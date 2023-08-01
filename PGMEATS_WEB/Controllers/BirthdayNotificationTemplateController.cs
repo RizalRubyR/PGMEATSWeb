258621ef0e2495f0a4c338ac11da61cdb3219744
MFadliHidayat <121424445+MFadliHidayat@users.noreply.github.com>
2023-08-01 15:22:47 +0700

    <Upd><BirthdayNotification>

---
+++ PGMEATS_WEB/Controllers/BirthdayNotificationTemplateController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PGMEATS_WEB.Models;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Configuration;
+using System.Net;
+using Newtonsoft.Json;
+using System.Web.Helpers;
+
+namespace PGMEATS_WEB.Controllers
+{
+    public class BirthdayNotificationTemplateController : Controller
+    {
+        public ActionResult Index()
+        {
+            /*CHECK SESSION LOGIN*/
+            if (Session["LogUserID"] is null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            string userID = Session["LogUserID"].ToString();
+            string AdminStatus = Session["AdminStatus"].ToString();
+            string MenuID = "F-01 ";
+
+            clsUserPrivilegeDB db = new clsUserPrivilegeDB();
+            clsUserPrivilege data = new clsUserPrivilege();
+            data.UserID = userID;
+            data.MenuID = MenuID;
+            data.AdminStatus = AdminStatus;
+
+            /*CHECK PRIVILEGE*/
+            clsUserPrivilege Privilege = db.UserPrivilegeCheck("3", data);
+            if (Privilege.AllowAccess == "0")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.AllowUpdate = Privilege.AllowUpdate;
+            ViewBag.UserID = userID;
+
+            return View();
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        [HttpPost]
+        public JsonResult GetBirthdayTemplateList()
+        {            
+            clsBirthdayNotificationTemplateDB db = new clsBirthdayNotificationTemplateDB();
+            clsResponse response = new clsResponse();
+            try
+            {
+                var user = Session["LogUserID"].ToString();
+                response = db.GetBirthdayNotificationTemplateList();
+            }
+            catch (Exception ex)
+            {
+                response.ID = 0;
+                response.Message = ex.Message;
+                response.Contents = "";
+            }
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        [HttpPost]
+        public JsonResult GetBirthdayTemplateListByID(string BirthdayTemplateID)
+        {
+            clsBirthdayNotificationTemplateDB db = new clsBirthdayNotificationTemplateDB();
+            clsResponse response = new clsResponse();
+            try
+            {
+                var user = Session["LogUserID"].ToString();
+                response = db.GetBirthdayNotificationTemplateListByBirthdayTemplateID(BirthdayTemplateID);
+            }
+            catch (Exception ex)
+            {
+                response.ID = 0;
+                response.Message = ex.Message;
+                response.Contents = "";
+            }
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        [HttpPost]
+        public JsonResult InsertData(clsBirthdayNotificationTemplate data)
+        {
+            data.CreateUser = Session["LogUserID"].ToString().Trim();
+            clsResponse response = new clsResponse();
+            clsBirthdayNotificationTemplateDB DB = new clsBirthdayNotificationTemplateDB();
+            try
+            {
+                response = DB.InsertUpdate(data);
+            }
+            catch (Exception ex)
+            {
+                response.ID = 0;
+                response.Message = ex.Message;
+                response.Contents = "";
+            }
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        [HttpPost]
+        public JsonResult DeleteData(clsBirthdayNotificationTemplate data)
+        {            
+            clsResponse response = new clsResponse();
+            clsBirthdayNotificationTemplateDB DB = new clsBirthdayNotificationTemplateDB();
+            try
+            {
+                response = DB.Delete(data);
+            }
+            catch (Exception ex)
+            {
+                response.ID = 0;
+                response.Message = ex.Message;
+                response.Contents = "";
+            }
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ PGMEATS_WEB/Models/clsBirthdayNotificationTemplate.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PGMEATS_WEB.Models
+{
+    public class clsBirthdayNotificationTemplate
+    {
+        public string BirthdayTemplateID { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public string CreateUser { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string UpdateUser { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+
+    public class clsBirthdayNotificationTemplateDB
+    {
+        public clsResponse GetBirthdayNotificationTemplateList()
+        {
+            List<clsBirthdayNotificationTemplate> BirthdayNotificationTemplateList = new List<clsBirthdayNotificationTemplate>();
+            clsResponse Response = new clsResponse();
+            try
+            {
+                string constr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_GET_BirthdayNotificationTemplate", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+                    da.Dispose();
+                    cmd.Dispose();
+                    con.Close();
+
+                    BirthdayNotificationTemplateList = dt.AsEnumerable().Select(x =>
+                    new clsBirthdayNotificationTemplate
+                    {
+                        BirthdayTemplateID = x.Field<Int32>("BirthdayTemplateID").ToString(),
+                        Subject = x.Field<string>("Subject"),
+                        Content = x.Field<string>("Content"),
+                    }).ToList();
+
+                    Response.ID = 1;
+                    Response.Message = "Success";
+                    Response.Contents = BirthdayNotificationTemplateList;
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.ID = 0;
+                Response.Message = ex.Message;
+                Response.Contents = "";
+
+            }
+            return Response;
+        }
+
+        public clsResponse GetBirthdayNotificationTemplateListByBirthdayTemplateID(string BirthdayTemplateID)
+        {
+            List<clsBirthdayNotificationTemplate> NewsList = new List<clsBirthdayNotificationTemplate>();
+            clsResponse Response = new clsResponse();
+            try
+            {
+                string constr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_GET_BirthdayNotificationTemplateByID", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("BirthdayTemplateID", BirthdayTemplateID);
+                    con.Open();
+
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+                    da.Dispose();
+                    cmd.Dispose();
+                    con.Close();
+
+                    List<string> data = new List<string>();
+                    data.Add(dt.Rows[0]["Subject"].ToString());
+                    data.Add(dt.Rows[0]["Content"].ToString());
+
+                    Response.ID = 1;
+                    Response.Message = "Success";
+                    Response.Contents = data;
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.ID = 0;
+                Response.Message = ex.Message;
+                Response.Contents = "";
+
+            }
+            return Response;
+        }
+
+        public clsResponse InsertUpdate(clsBirthdayNotificationTemplate data)
+        {
+            clsResponse Response = new clsResponse();
+            try
+            {
+                string constr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_Ins_BirthdayNotificationTemplate", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("BirthdayTemplateID", data.BirthdayTemplateID ?? "");
+                    cmd.Parameters.AddWithValue("Subject", data.Subject ?? "");
+                    cmd.Parameters.AddWithValue("Content", data.Content ?? "");
+                    cmd.Parameters.AddWithValue("UserCreate", data.CreateUser ?? "");
+
+                    con.Open();
+
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+                    da.Dispose();
+                    cmd.Dispose();
+                    con.Close();
+
+                    Response.ID = 1;
+                    Response.Message = dt.Rows[0]["Message"].ToString();
+                    Response.Contents = dt.Rows[0]["ID"].ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.ID = 0;
+                Response.Message = ex.Message;
+                Response.Contents = "";
+            }
+
+            return Response;
+        }
+
+        public clsResponse Delete(clsBirthdayNotificationTemplate data)
+        {
+            clsResponse Response = new clsResponse();
+            try
+            {
+                string constr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_BirthdayTemplate_Delete", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("BirthdayTemplateID", data.BirthdayTemplateID);
+                    con.Open();
+
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+                    da.Dispose();
+                    cmd.Dispose();
+                    con.Close();
+
+                    Response.ID = 1;
+                    Response.Message = dt.Rows[0]["Message"].ToString();
+                    Response.Contents = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.ID = 0;
+                Response.Message = ex.Message;
+                Response.Contents = "";
+            }
+
+            return Response;
+        }
+    }
+}
