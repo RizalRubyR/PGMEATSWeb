4f5efd3167fed57f1ff3775628776bb2d7f3e6a7
MalikIlman <98869430+MalikIlman@users.noreply.github.com>
2024-01-29 16:51:33 +0700

    <Add><SportMaster><New Menu>

---
+++ PGMEATS_WEB/Controllers/SportMasterController.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PGMEATS_WEB.Models;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Configuration;
+using System.Net;
+using Newtonsoft.Json;
+using System.Web.Helpers;
+
+namespace PGMEATS_WEB.Controllers
+{
+    public class SportMasterController : Controller
+    {
+        public ActionResult Index()
+        {
+            /*CHECK SESSION LOGIN*/
+            if (Session["LogUserID"] is null)
+            {
+                return RedirectToAction("Login", "Home");
+            }
+
+            string userID = Session["LogUserID"].ToString();
+            string AdminStatus = Session["AdminStatus"].ToString();
+            string MenuID = "H-01";
+
+            clsUserPrivilegeDB db = new clsUserPrivilegeDB();
+            clsUserPrivilege data = new clsUserPrivilege();
+            data.UserID = userID;
+            data.MenuID = MenuID;
+            data.AdminStatus = AdminStatus;
+
+            /*CHECK PRIVILEGE*/
+            clsUserPrivilege Privilege = db.UserPrivilegeCheck("3", data);
+            if (Privilege.AllowAccess == "0")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.AllowUpdate = Privilege.AllowUpdate;
+            ViewBag.UserID = userID;
+
+            return View();
+        }
+
+
+        [AcceptVerbs("GET", "POST")]
+        [HttpPost]
+        public JsonResult SportMasterList()
+        {
+            List<clsSportMaster> data = new List<clsSportMaster>();
+            clsSportMasterDB db = new clsSportMasterDB();
+            clsResponse response = new clsResponse();
+            try
+            {
+                response = db.SportMasterList();
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+            }
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        [HttpPost]
+        public JsonResult FillCombo(String Type)
+        {
+            List<clsSportMaster> data = new List<clsSportMaster>();
+            clsSportMasterDB db = new clsSportMasterDB();
+            clsResponse response = new clsResponse();
+            try
+            {
+                response = db.FillCombo(Type);
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+            }
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        [HttpPost]
+        public JsonResult SportMasterIns(clsSportMaster data)
+
+        {
+            data.LastUser = Session["LogUserID"].ToString();
+            clsResponse response = new clsResponse();
+            clsSportMasterDB DB = new clsSportMasterDB();
+            try
+            {
+                response = DB.SportMasterIns(data);
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+            }
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        [HttpPost]
+        public JsonResult SportMasterUpd(clsSportMaster data)
+
+        {
+            data.LastUser = Session["LogUserID"].ToString();
+            clsResponse response = new clsResponse();
+            clsSportMasterDB DB = new clsSportMasterDB();
+            try
+            {
+                response = DB.SportMasterUpd(data, "1");
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+            }
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+
+
+        [AcceptVerbs("GET", "POST")]
+        [HttpPost]
+        public JsonResult SportMasterDel(String SportID)
+        {
+            clsResponse response = new clsResponse();
+            clsSportMasterDB DB = new clsSportMasterDB();
+            try
+            {
+                string filename = "IMG" + SportID.Trim();
+                response = DB.SportMasterDel(SportID);
+
+                if (response.Message.ToLower().Contains("success"))
+                {
+                    clsConPathFolderDB dbPath = new clsConPathFolderDB();
+                    string PathWeb = dbPath.PathFolder("Web", "SportMaster"); //Jika Web
+                    string PathMobile = dbPath.PathFolder("Mobile", "SportMaster"); //Jika Mobile
+
+                    string date = DateTime.Now.ToString("yyyyMMddHHmmss");
+
+                    if (!Directory.Exists(PathWeb)) { Directory.CreateDirectory(PathWeb); } //jika path web tidak ditemukan makan buat path            
+                    if (!Directory.Exists(PathMobile)) { Directory.CreateDirectory(PathMobile); } //jika path mobile tidak ditemukan makan buat path
+
+                    var ImgWeb = System.IO.Directory.GetFiles(PathWeb + @"\", "*" + filename + "*.PNG"); //get file Web
+                    var ImgMobile = System.IO.Directory.GetFiles(PathMobile + @"\", "*" + filename + "*.PNG"); //get file Mobile
+
+                    //jika filenya ada maka hapus file web
+                    if (ImgWeb.Length > 0)
+                    {
+                        for (int i = 0; i <= ImgWeb.Length - 1; i++)
+                        {
+                            var LastImgPath = ImgWeb[i];
+                            var LastImgName = System.IO.Path.GetFileName(LastImgPath);
+                            var LastImgFullPath = PathWeb + LastImgName;
+                            System.IO.File.Delete(LastImgFullPath);
+                        }
+                    }
+
+                    //jika filenya ada maka hapus file mobile
+                    if (ImgMobile.Length > 0)
+                    {
+                        for (int i = 0; i <= ImgMobile.Length - 1; i++)
+                        {
+                            var LastImgPath = ImgMobile[i];
+                            var LastImgName = System.IO.Path.GetFileName(LastImgPath);
+                            var LastImgFullPath = PathMobile + LastImgName;
+                            System.IO.File.Delete(LastImgFullPath);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+            }
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        [HttpPost]
+        public JsonResult ImgSave(clsSportMaster data)
+        {
+            clsResponse response = new clsResponse();
+            clsSportMasterDB DB = new clsSportMasterDB();
+            try
+            {
+                clsConPathFolderDB dbPath = new clsConPathFolderDB();
+                string PathWeb = dbPath.PathFolder("Web", "SportMaster"); //Jika Web
+                string PathMobile = dbPath.PathFolder("Mobile", "SportMaster"); //Jika Mobile 
+
+                string date = DateTime.Now.ToString("yyyyMMddHHmmss");
+                string fileName = "IMG" + data.SportID.Trim();
+                //string fileName = data.SportMasterID.Trim();
+
+
+                byte[] bytes = Convert.FromBase64String(data.files);
+                WebImage img = new WebImage(bytes);
+                if (img.Width > 1000) { img.Resize(1000, 1000); }  //convert image size
+
+                if (PathWeb != "")
+                {
+                    if (!Directory.Exists(PathWeb)) { Directory.CreateDirectory(PathWeb); } //jika path web tidak ditemukan makan buat path
+                    var ImgWeb = System.IO.Directory.GetFiles(PathWeb + @"\", "*" + fileName + "*.PNG"); //get file Web
+
+                    //jika filenya ada maka hapus file web
+                    if (ImgWeb.Length > 0)
+                    {
+                        for (int i = 0; i <= ImgWeb.Length - 1; i++)
+                        {
+                            var LastImgPath = ImgWeb[i];
+                            var LastImgName = System.IO.Path.GetFileName(LastImgPath);
+                            var LastImgFullPath = PathWeb + LastImgName;
+                            System.IO.File.Delete(LastImgFullPath);
+                        }
+                    }
+
+                    var ImgPathWeb = PathWeb + fileName; //file name web
+                    img.Save(ImgPathWeb, "PNG"); //save to web
+                }
+
+                if (PathMobile != "")
+                {
+                    if (!Directory.Exists(PathMobile)) { Directory.CreateDirectory(PathMobile); } //jika path mobile tidak ditemukan makan buat path
+                    var ImgMobile = System.IO.Directory.GetFiles(PathMobile + @"\", "*" + fileName + "*.PNG"); //get file Mobile
+
+                    //jika filenya ada maka hapus file mobile
+                    if (ImgMobile.Length > 0)
+                    {
+                        for (int i = 0; i <= ImgMobile.Length - 1; i++)
+                        {
+                            var LastImgPath = ImgMobile[i];
+                            var LastImgName = System.IO.Path.GetFileName(LastImgPath);
+                            var LastImgFullPath = PathMobile + LastImgName;
+                            System.IO.File.Delete(LastImgFullPath);
+                        }
+                    }
+
+                    var ImgPathMobile = PathMobile + fileName; //file name mobile
+                    img.Save(ImgPathMobile, "PNG"); //save to mobile
+
+                }
+
+                //update filename to db
+                var FileName = fileName + ".PNG";
+                data.LastUser = Session["LogUserID"].ToString();
+                data.FileName = FileName;
+                response = DB.SportMasterUpd(data, "1");
+            }
+
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+            }
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        [HttpPost]
+        public JsonResult ImgSaveAfterClick(clsSportMaster data)
+        {
+            clsResponse response = new clsResponse();
+            clsSportMasterDB DB = new clsSportMasterDB();
+            try
+            {
+                clsConPathFolderDB dbPath = new clsConPathFolderDB();
+                string PathWeb = dbPath.PathFolder("Web", "SportMaster"); //Jika Web
+                string PathMobile = dbPath.PathFolder("Mobile", "SportMaster"); //Jika Mobile 
+
+                string date = DateTime.Now.ToString("yyyyMMddHHmmss");
+                string fileName = "IMG" + data.SportID.Trim() + "_Clicked";
+                //string fileName = data.SportMasterID.Trim();
+
+
+                byte[] bytes = Convert.FromBase64String(data.files);
+                WebImage img = new WebImage(bytes);
+                if (img.Width > 1000) { img.Resize(1000, 1000); }  //convert image size
+
+                if (PathWeb != "")
+                {
+                    if (!Directory.Exists(PathWeb)) { Directory.CreateDirectory(PathWeb); } //jika path web tidak ditemukan makan buat path
+                    var ImgWeb = System.IO.Directory.GetFiles(PathWeb + @"\", "*" + fileName + "*.PNG"); //get file Web
+
+                    //jika filenya ada maka hapus file web
+                    if (ImgWeb.Length > 0)
+                    {
+                        for (int i = 0; i <= ImgWeb.Length - 1; i++)
+                        {
+                            var LastImgPath = ImgWeb[i];
+                            var LastImgName = System.IO.Path.GetFileName(LastImgPath);
+                            var LastImgFullPath = PathWeb + LastImgName;
+                            System.IO.File.Delete(LastImgFullPath);
+                        }
+                    }
+
+                    var ImgPathWeb = PathWeb + fileName; //file name web
+                    img.Save(ImgPathWeb, "PNG"); //save to web
+                }
+
+                if (PathMobile != "")
+                {
+                    if (!Directory.Exists(PathMobile)) { Directory.CreateDirectory(PathMobile); } //jika path mobile tidak ditemukan makan buat path
+                    var ImgMobile = System.IO.Directory.GetFiles(PathMobile + @"\", "*" + fileName + "*.PNG"); //get file Mobile
+
+                    //jika filenya ada maka hapus file mobile
+                    if (ImgMobile.Length > 0)
+                    {
+                        for (int i = 0; i <= ImgMobile.Length - 1; i++)
+                        {
+                            var LastImgPath = ImgMobile[i];
+                            var LastImgName = System.IO.Path.GetFileName(LastImgPath);
+                            var LastImgFullPath = PathMobile + LastImgName;
+                            System.IO.File.Delete(LastImgFullPath);
+                        }
+                    }
+
+                    var ImgPathMobile = PathMobile + fileName; //file name mobile
+                    img.Save(ImgPathMobile, "PNG"); //save to mobile
+
+                }
+
+                //update filename to db
+                var FileName = fileName + ".PNG";
+                data.LastUser = Session["LogUserID"].ToString();
+                data.FileName = FileName;
+                response = DB.SportMasterUpd(data, "2");
+            }
+
+            catch (Exception ex)
+            {
+                response.Message = ex.Message;
+            }
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult GetFillCombo(String TypeAction, String Param)
+        {
+            Response resp = new Response();
+            ComboFilter db = new ComboFilter();
+            try
+            {
+                resp = db.FillCombo(TypeAction, Param);
+                return Json(resp, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                resp.Message = ex.Message;
+                resp.ID = "1";
+                resp.Content = "";
+
+                return Json(resp, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+    }
+}
---
+++ PGMEATS_WEB/Models/clsSportMaster.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PGMEATS_WEB.Models
+{
+    public class clsSportMaster
+    {
+        public string SportID { get; set; }
+        public string SportDesc { get; set; }
+        public string ActiveStatus { get; set; }
+        public string LastUser { get; set; }
+        public string LastUpdate { get; set; }
+        public string FileName { get; set; }
+        public string files { get; set; }
+        public string FileName2 { get; set; }
+        public string files2 { get; set; }
+    }
+
+    public class clsSportMasterDB
+    {
+        public clsResponse SportMasterList()
+        {
+            List<clsSportMaster> Menus = new List<clsSportMaster>();
+           clsResponse Response = new clsResponse();
+            try
+            {
+                string constr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_SportMaster_GetList", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+
+                    SqlDataReader rd = cmd.ExecuteReader();
+                    while (rd.Read())
+                    {
+                        clsSportMaster Menu = new clsSportMaster();
+                        Menu.SportID = rd["SportID"].ToString();
+                        Menu.SportDesc = rd["SportDesc"].ToString();
+                        Menu.ActiveStatus = rd["ActiveStatus"].ToString();
+                        Menu.LastUser = rd["UpdateUser"].ToString();
+                        Menu.LastUpdate = rd["UpdateDate"].ToString();
+                        Menu.FileName = rd["FileName"].ToString();
+                        Menus.Add(Menu);
+                    }
+
+                    Response.Message = "Success";
+                    Response.Contents = Menus;                
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.ID = 0;
+                Response.Message = ex.Message;
+                Response.Contents = "";
+                
+            }
+            return Response;
+        }
+
+        public clsResponse SportMasterIns(clsSportMaster dataFrom)
+        {
+            clsResponse Response = new clsResponse();
+            clsSportMaster data = new clsSportMaster();
+            try
+            {
+                string constr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_SportMaster_Ins", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("SportID", dataFrom.SportID ?? "");
+                    cmd.Parameters.AddWithValue("SportDescription", dataFrom.SportDesc ?? "");
+                    cmd.Parameters.AddWithValue("ActiveStatus", dataFrom.ActiveStatus ?? "");
+                    cmd.Parameters.AddWithValue("UserID", dataFrom.LastUser ?? "");
+                    con.Open();
+
+                    SqlDataReader rd = cmd.ExecuteReader();
+                    while (rd.Read())
+                    {
+                        Response.Contents = rd["SportID"].ToString();
+                    }
+
+                    Response.Message = "Successfully Saved data!";
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Message = ex.Message;
+            }
+
+            return Response;
+        }
+
+        public clsResponse SportMasterUpd(clsSportMaster dataFrom, string TypeUpdate)
+        {
+            clsResponse Response = new clsResponse();
+            clsSportMaster data = new clsSportMaster();
+            try
+            {
+                string constr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_SportMaster_Upd", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("SportID", dataFrom.SportID ?? "");
+                    if(TypeUpdate == "1")
+                    {
+                        cmd.Parameters.AddWithValue("SportDescription", dataFrom.SportDesc ?? "");
+                        cmd.Parameters.AddWithValue("ActiveStatus", dataFrom.ActiveStatus ?? "");
+                        cmd.Parameters.AddWithValue("FileName", dataFrom.FileName ?? "");
+                        cmd.Parameters.AddWithValue("TypeUpdate", TypeUpdate);
+                    }
+                    else if(TypeUpdate == "2")
+                    {
+                        cmd.Parameters.AddWithValue("FileName2", dataFrom.FileName2 ?? "");
+                        cmd.Parameters.AddWithValue("TypeUpdate", TypeUpdate);
+                    }
+                    cmd.Parameters.AddWithValue("UserID", dataFrom.LastUser ?? "");
+                    con.Open();
+
+                    SqlDataReader rd = cmd.ExecuteReader();
+                    while (rd.Read())
+                    {
+                        Response.Contents = rd["SportID"].ToString();
+                    }
+
+                    Response.Message = "Successfully updated data!";
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Message = ex.Message;
+            }
+
+            return Response;
+        }
+
+        public clsResponse SportMasterDel(string SportID)
+        {
+            clsResponse Response = new clsResponse();
+            try
+            {
+                string constr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_SportMaster_Del", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("SportID", SportID);
+                    con.Open();
+
+                    DataTable dt = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+                    da.Dispose();
+                    cmd.Dispose();
+                    con.Close();
+
+                    Response.Message = dt.Rows[0]["Msg"].ToString();
+          
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Message = ex.Message;
+            }
+
+            return Response;
+        }
+
+        public clsResponse FillCombo(string Type)
+        {
+            clsResponse Response = new clsResponse();
+            List<clsFillCombo> fillcombos = new List<clsFillCombo>();
+            try
+            {
+                string constr = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+                    SqlCommand cmd = new SqlCommand("sp_FilterCombo", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("Type", Type);
+                    con.Open();
+
+                    SqlDataReader rd = cmd.ExecuteReader();
+                    while (rd.Read())
+                    {
+                        clsFillCombo fillcombo = new clsFillCombo();
+                        fillcombo.Code = rd["Code"].ToString();
+                        fillcombo.Description = rd["Description"].ToString();
+                        fillcombos.Add(fillcombo);
+                    }
+                    Response.Message = "Success";
+                    Response.Contents = fillcombos;
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Message = ex.Message;
+            }
+            return Response;
+        }
+
+    }
+}
