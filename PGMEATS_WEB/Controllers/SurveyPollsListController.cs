5d77755a88ed66a714ed6ac8bd2cbe1f39d81b3e
MalikIlman <98869430+MalikIlman@users.noreply.github.com>
2024-12-04 15:08:10 +0700

    <upd><SurveyAndPolls><Export Excel>

--- PGMEATS_WEB/Controllers/SurveyPollsListController.cs
+++ PGMEATS_WEB/Controllers/SurveyPollsListController.cs
@@ -14,6 +14,8 @@ using System.Web.Helpers;
 using System.Data;
 using System.Collections;
 using System.Text;
+using OfficeOpenXml;
+using OfficeOpenXml.Drawing.Chart;
 
 namespace PGMEATS_WEB.Controllers
 {
@@ -133,7 +135,6 @@ namespace PGMEATS_WEB.Controllers
 
             string userID = Session["LogUserID"].ToString();
             string AdminStatus = Session["AdminStatus"].ToString();
-
             List<CheckResult> resp = new List<CheckResult>();
             List<checkResult> check = new List<checkResult>();
             List<checkResult> Responses = new List<checkResult>();
@@ -156,6 +157,7 @@ namespace PGMEATS_WEB.Controllers
             ViewBag.InfoUser = resultInfo[0].CreateBy + " | " + DateTime.Now.ToString("dd MMM yyyy");
 
             ViewBag.Responses = Responses[0].Messages; //"50 Responses";
+            Session["SurveyTitle"] = resultInfo[0].SurveyTitle;
             return View();
         }
 
@@ -912,6 +914,167 @@ namespace PGMEATS_WEB.Controllers
         //    }
         //    return fname;
         //}
+
+        public ActionResult ExportToExcel()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            // Sample data (you can replace this with actual data from your database or model)
+            //SurveyAndPollsDB db = new SurveyAndPollsDB();
+            //Encryption enc = new Encryption();
+            //string param = enc.EncryptData("45" + "||");
+            //var data = db.GetchartByShift(param).ToList();
+
+            //// Create a memory stream to save the file in memory
+            ////using (var package = new ExcelPackage())
+            ////{
+            ////    var worksheet = package.Workbook.Worksheets.Add("Data");
+            ////    worksheet.Cells["A1"].LoadFromCollection(data, true);
+
+            ////    // Convert the data to an Excel file in memory
+            ////    var stream = new MemoryStream(package.GetAsByteArray());
+
+            ////    // Return the Excel file to the user
+            ////    return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "DataExport.xlsx");
+            ////}
+
+            using (var package = new ExcelPackage())
+            {
+                // Add a worksheet to the package
+                var worksheet = package.Workbook.Worksheets.Add("Survey & Polls");
+
+                // Add headers
+                worksheet.Cells[2, 1].Value = Session["SurveyTitle"].ToString();
+                worksheet.Cells[2, 1].Style.Font.Bold = true;
+                worksheet.Cells[4, 1].Value = "By Department";
+                worksheet.Cells[4, 1].Style.Font.Bold = true;
+                var Picture = worksheet.Drawings.AddPicture("byDept", new FileInfo(Path.Combine(Server.MapPath("~/Image/SurveyAndPolls"), "byDept.png"))); 
+                Picture.SetPosition(5, 0, 1, 0);
+                Picture.SetSize(1000, 800);        // Set image size (100px x 100px)
+
+                var Picture2 = worksheet.Drawings.AddPicture("bydepartment", new FileInfo(Path.Combine(Server.MapPath("~/Image/SurveyAndPolls"), "bydepartment.png")));
+                Picture2.SetPosition(49, 0, 1, 0);
+                Picture2.SetSize(1000, 800);        // Set image size (100px x 100px)
+
+                worksheet.Cells[93, 1].Value = "By Staff";
+                worksheet.Cells[93, 1].Style.Font.Bold = true;
+
+                var Picture3 = worksheet.Drawings.AddPicture("byShift", new FileInfo(Path.Combine(Server.MapPath("~/Image/SurveyAndPolls"), "byShift.png")));
+                Picture3.SetPosition(95, 0, 1, 0);
+                Picture3.SetSize(1000, 800);        // Set image size (100px x 100px)
+
+                var Picture4 = worksheet.Drawings.AddPicture("Shift", new FileInfo(Path.Combine(Server.MapPath("~/Image/SurveyAndPolls"), "Shift.png")));
+                Picture4.SetPosition(139, 0, 1, 0);
+                Picture4.SetSize(1000, 800);        // Set image size (100px x 100px)
+
+                // Insert an image into the worksheet from a URL
+                //string imageUrl = "https://example.com/path/to/logo.png";  // Replace with the actual image URL
+                //var imageBytes = DownloadImage(imageUrl); // Download image to byte array
+
+                //// Insert the image into the worksheet at a specific position
+                //var picture = worksheet.Drawings.AddPicture("Logo", imageBytes);
+                //picture.SetPosition(1, 0, 5, 0);  // Set position (row 1, col 5)
+                //picture.SetSize(100, 100);        // Set image size (100px x 100px)
+
+                // Generate the file as a byte array
+                var fileContents = package.GetAsByteArray();
+
+                // Return the file to the client
+                return File(fileContents, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", ""+ Session["SurveyTitle"].ToString() + ".xlsx");
+            }
+        }
+        private byte[] DownloadImage(string imageUrl)
+        {
+            using (var webClient = new WebClient())
+            {
+                return webClient.DownloadData(imageUrl);
+            }
+        }
+        [HttpPost]
+        public ActionResult ExportToExcel2(object data)
+        {
+            // Generate the content to be exported (could be dynamic data or static content)
+            //var content = RenderViewToString("Result");
+
+            //// Generate the Excel file from the content
+            //using (var package = new ExcelPackage())
+            //{
+            //    var worksheet = package.Workbook.Worksheets.Add("Sheet1");
+
+            //    // Assuming content is HTML-based (can be replaced with any structured data)
+            //    var row = 1;
+            //    var col = 1;
+
+            //    // Split the content into rows and columns (simple implementation)
+            //    var lines = content.Split(new[] { "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            //    foreach (var line in lines)
+            //    {
+            //        worksheet.Cells[row, col].Value = line;
+            //        row++;
+            //    }
+
+            //    // Convert the Excel package to a byte array
+            //    var stream = new MemoryStream(package.GetAsByteArray());
+
+            //    // Return the Excel file as a downloadable file
+            //    return File(stream.ToArray(), "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "ExportedContent.xlsx");
+            //}
+
+            // Parse the incoming JSON data
+            var jsonData = Request.InputStream;
+            var reader = new StreamReader(jsonData);
+            var content = reader.ReadToEnd();
+            var chartData = JsonConvert.DeserializeObject<dynamic>(content);
+
+            // Get the base64-encoded chart image data from the client
+            string imageBase64 = chartData.chartImage;
+
+            // Decode the base64 image data
+            byte[] imageBytes = Convert.FromBase64String(imageBase64.Substring(imageBase64.IndexOf(",") + 1));
+
+            // Save the image to the server (in a specific folder like App_Data)
+            string imagePath = Path.Combine(Server.MapPath("~/App_Data"), "chart_image.png");
+            System.IO.File.WriteAllBytes(imagePath, imageBytes);
+
+            // Create an Excel package
+            using (var package = new ExcelPackage())
+            {
+                // Add a worksheet
+                var worksheet = package.Workbook.Worksheets.Add("Chart Data");
+
+                // Insert the image into the Excel worksheet
+                var image = worksheet.Drawings.AddPicture("ChartImage", imagePath);
+                image.SetPosition(1, 0, 1, 0);  // Set the position of the image in the worksheet
+
+                // Prepare the Excel file to send to the user
+                var fileBytes = package.GetAsByteArray();
+
+                // Return the Excel file for download
+                return File(fileBytes, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "ChartDataWithImage.xlsx");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult SaveImage(ImageData model)
+        {
+            // Decode the Base64 string
+            string[] ModelNew = model.Image.ToString().Split(new[] { "||" }, StringSplitOptions.None);
+            model.Image = ModelNew[1];
+            string base64String = model.Image.Replace("data:image/png;base64,", "");
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+
+            // Define the path where you want to save the image
+            string filePath = Path.Combine(Server.MapPath("~/Image/SurveyAndPolls"), ModelNew[0] + ".png");
+
+            // Save the image to the server
+            System.IO.File.WriteAllBytes(filePath, imageBytes);
+
+            return Json(new { success = true });
+        }
+
+        public class ImageData
+        {
+            public string Image { get; set; }
+        }
 
     }
 }
